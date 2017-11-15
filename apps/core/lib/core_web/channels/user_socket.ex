12f6aee7d305a0382fffb9acd75eddd14bc6e6c4
Łukasz Jankowski <lukaszpjankowski@gmail.com>
2017-11-15 23:16:15 +0100

    remove commented code

--- apps/core/lib/core_web/channels/user_socket.ex
+++ apps/core/lib/core_web/channels/user_socket.ex
@@ -5,21 +5,6 @@ defmodule CoreWeb.UserSocket do
 
   transport :websocket, Phoenix.Transports.WebSocket
 
-  # def connect(params, socket) do
-  #   current_user = current_user(params)
-  #   socket = Absinthe.Phoenix.Socket.put_opts(socket, context: %{
-  #     current_user: current_user
-  #   })
-  #   {:ok, socket}
-  # end
-
-  # defp current_user(%{"user_id" => id}) do
-  #   MyApp.Repo.get(User, id)
-  # end
-  # def connect(_params, socket) do
-  #   {:ok, assign(socket, :absinthe, %{schema: CoreWeb.Schema})}
-  # end
-
   def connect(_params, socket) do
     {:ok, socket}
   end
